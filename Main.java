5b33f3fb52ba265d5097bd6bd1fec7ef80eca8d6
Anton Podobytko <anton.podobytko@gmail.com>
2021-03-27 15:51:47 +0300

    reformat

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 package stationdm.euapi.header;
+
 import java.util.Base64;
 
 public class RemoteHttpHeader {
@@ -7,12 +8,12 @@ public class RemoteHttpHeader {
 	}
 
 	public static void main(String[] args) {
-		if(args.length == 0) {
+		if (args.length == 0) {
 			System.out.println("Please give an input");
 			return;
 		}
 
-		if(args.length != 2) {
+		if (args.length != 2) {
 			return;
 		}
 
@@ -26,7 +27,7 @@ public class RemoteHttpHeader {
 
 		if (command.equalsIgnoreCase("list")) {
 			System.out.println("Generating 1k stamps...");
-			for(int i=0; i<1000; i++) {
+			for (int i = 0; i < 1000; i++) {
 				int unixTime = (int) (System.currentTimeMillis() / 1000L) + i;
 				String stamp = getStamp(clientId + ":" + unixTime);
 				System.out.println(stamp);
